0513c73844e8ac5a701b92bf03a4b2da1db48a8b
Leonardo Reyes <leonardo.reyes@colfuturo.org>
2019-12-18 07:01:19 -0500

    Configuration with properties

---
+++ src/main/java/com/in28minutes/microservices/limitsservices/bean/LimitConfiguration.java
@@ -0,0 +1,23 @@
+package com.in28minutes.microservices.limitsservices.bean;
+
+public class LimitConfiguration {
+	
+	private int maximum;
+	private int minimum;
+	
+	public LimitConfiguration() {
+		super();
+	}
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+
+}

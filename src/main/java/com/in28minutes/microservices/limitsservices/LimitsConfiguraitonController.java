5f7f2b85fc51070fe9eb3fe416bef3fd767ba1e0
Leonardo Reyes <leonardo.reyes@colfuturo.org>
2019-12-26 21:12:48 -0500

    hystrix to manage failures in a string of sevices

--- src/main/java/com/in28minutes/microservices/limitsservices/LimitsConfiguraitonController.java
+++ src/main/java/com/in28minutes/microservices/limitsservices/LimitsConfiguraitonController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservices.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfiguraitonController {
@@ -15,6 +16,16 @@ public class LimitsConfiguraitonController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations(){
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallBackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration(){
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallBackRetrieveConfiguration(){
+		return new LimitConfiguration(999,9);
 	}
 
 }
--- src/main/java/com/in28minutes/microservices/limitsservices/LimitsServicesApplication.java
+++ src/main/java/com/in28minutes/microservices/limitsservices/LimitsServicesApplication.java
@@ -2,8 +2,10 @@ package com.in28minutes.microservices.limitsservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class LimitsServicesApplication {
 
 	public static void main(String[] args) {
